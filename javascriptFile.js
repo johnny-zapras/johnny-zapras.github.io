e9fe57db1aaf7a2b133f40ad509002eff618efa9
Johnny Z <jzapras>
2017-05-03 16:01:20 -0400

    Initial experimentation with resume website

---
+++ javascriptFile.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function navAndName(){
+	document.getElementById("menuBar").innerHTML =
+	'<a href="firstPage.html">Home</a>' + 
+	'<a href="index.html">Other Page</a>' +
+	'</ br>' +
+	'<h1 id="title">Johnny Zapras</h1>';
+}, false);
