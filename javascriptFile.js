c9721a7539174ae882013a7ac93b802f7ebac8bd
Johnny Z <jzapras>
2017-06-25 22:34:16 -0400

    Adding dynamic list for menu creation.

--- javascriptFile.js
+++ javascriptFile.js
@@ -1,7 +1,15 @@
+var menuOptions = ['Home', 'About', 'Portfolio', 'Contact'];
+
+
+function createNavBar(navigationBar){
+	for(var i=0; i<menuOptions.length; i++){
+		var listItem = document.create('li');
+		listItem.innerHTML = menuOptions[i];
+		navigationBar.appendChild(listItem);
+	}
+}
+
 window.addEventListener("load", function navAndName(){
-	document.getElementById("menuBar").innerHTML =
-	'<a href="firstPage.html">Home</a>' + 
-	'<a href="index.html">Other Page</a>' +
-	'</ br>' +
-	'<h1 id="title">Johnny Zapras</h1>';
+	var navBar = document.getElementById("menuBar").innerHTML;
+	createNavBar(navBar);
 }, false);
