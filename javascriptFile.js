86212fd3bc7d093a129d6b70bd3d422684bf963d
Johnny Z <jzapras>
2017-08-31 01:36:03 -0400

    it's musical now baby

--- javascriptFile.js
+++ javascriptFile.js
@@ -5,6 +5,10 @@ function createNavBar(navigationBar){
 	for(var i=0; i<menuOptions.length; i++){
 		var listItem = document.createElement('li');
 		listItem.innerHTML = menuOptions[i];
+		listItem.id = "menuItems";
+		var associatedLink = document.createElement('a');
+		associatedLink.href = "currentPage.html";
+		listItem.appendChild(associatedLink);
 		navigationBar.appendChild(listItem);
 	}
 }
